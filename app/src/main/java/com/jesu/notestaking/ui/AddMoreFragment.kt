39a79218f44be1d67491879e6cb1aba70da973fa
je$u <jesuvijayraj8246@gmail.com>
2020-06-07 20:15:28 +0530

    learn navigation, recycler view,Room database and material components

--- app/src/main/java/com/jesu/notestaking/ui/AddMoreFragment.kt
+++ app/src/main/java/com/jesu/notestaking/ui/AddMoreFragment.kt
@@ -1,11 +1,10 @@
 package com.jesu.notestaking.ui
 
+import android.app.AlertDialog
 import android.os.AsyncTask
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import android.widget.Toast
 import androidx.navigation.Navigation
 import com.jesu.notestaking.R
@@ -22,6 +21,7 @@ class AddMoreFragment : BaseFragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        setHasOptionsMenu(true)
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_add_more, container, false)
     }
@@ -32,7 +32,9 @@ class AddMoreFragment : BaseFragment() {
             note = AddMoreFragmentArgs.fromBundle(it).note
             et_title.setText(note?.title)
             et_desc.setText(note?.note)
+
         }
+
         btn_done.setOnClickListener { view ->
             val noteTitle = et_title.text.toString().trim()
             val noteDesc = et_desc.text.toString().trim()
@@ -53,9 +55,9 @@ class AddMoreFragment : BaseFragment() {
                     val mNote = Note(noteTitle, noteDesc)
                     if (note == null) {
                         NotesDatabase(it).getNoteeDao().addNote(mNote)
-                                it.toast("Note saved")
+                        it.toast("Note saved")
                     } else {
-                        mNote.id=note!!.id
+                        mNote.id = note!!.id
                         NotesDatabase(it).getNoteeDao().updateNote(mNote)
                         it.toast("Note updated")
                     }
@@ -70,7 +72,43 @@ class AddMoreFragment : BaseFragment() {
 
     }
 
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
 
+        when (item.itemId) {
+            R.id.menu_delete -> {
+                if (note != null) {
+                    deleteNote()
+                } else {
+                    context?.toast("cannote delete data")
+                }
+            }
+
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun deleteNote() {
+        AlertDialog.Builder(context).apply {
+            setTitle("Are you sure?")
+            setMessage("You cannot undo this operation")
+            setPositiveButton("yes") { _, _ ->
+
+                launch {
+                    NotesDatabase(context).getNoteeDao().deleteNote(note!!)
+                    val action = AddMoreFragmentDirections.actionSavemore()
+                    Navigation.findNavController(requireView()).navigate(action)
+                }
+
+            }
+            setNegativeButton("no") { _, _ ->
+            }
+        }.create().show()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.home, menu)
+    }
     // save note
 //    private fun saveNote(note: Note) {
 //
